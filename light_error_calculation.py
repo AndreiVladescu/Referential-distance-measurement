def8cb08132841b5f87315103550bc472f969703
AndreiVladescu <rudolfal2lea@yahoo.com>
2022-12-12 08:18:47 +0200

    Light Error Init

---
+++ light_error_calculation.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
+import numpy as np
+import pyzed.sl as sl
+import cv2
+import math
